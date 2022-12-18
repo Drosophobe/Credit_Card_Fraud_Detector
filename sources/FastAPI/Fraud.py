9ef226ab6ed7d9491db92e57f1198707fa0843bf
Drosophobe <sarzier.pierre@hotmail.fr>
2022-12-18 22:23:14 +0100

    db_files removed and Fraud.py added

---
+++ sources/FastAPI/Fraud.py
@@ -0,0 +1,34 @@
+import pickle
+import pandas as pd
+
+# Load models
+loaded_model_partial = pickle.load(open('dt_partial.sav', 'rb'))
+loaded_model_i = pickle.load(open('dt_i.sav', 'rb'))
+loaded_model_full = pickle.load(open('dt_full.sav', 'rb'))
+#scaler=pickle.load(open("scaler.pkl", 'rb'))
+
+# Fraud function
+'''
+Returns 'Fraud' or 'No Fraud' with the following Inputs_list
+['distance_from_home',
+'distance_from_last_transaction',
+'ratio_to_median_purchase_price',
+'repeat_retailer',
+'used_chip',
+'used_pin_number',
+'online_order'
+'''
+
+def Fraud(Input_list, model):
+    Fraud_eval=pd.DataFrame([Input_list],columns=['distance_from_home',
+                               'distance_from_last_transaction',
+                               'ratio_to_median_purchase_price',
+                               'repeat_retailer',
+                               'used_chip',
+                               'used_pin_number',
+                               'online_order'])
+    result=model.predict(Fraud_eval)
+    if result[0]==0:
+        return ("No Fraud")
+    else:
+        return ("Fraud")
